fef4637aef997bd0f280eb491ec00eae6e96e8b3
daoha20102003 gmail.com <daoha20102003@gmail.com>
2025-08-19 12:15:36 +0000

    Updated Program.cs

--- ShareItAPI/Program.cs
+++ ShareItAPI/Program.cs
@@ -305,7 +305,7 @@ namespace ShareItAPI
 
 
 
-            // builder.WebHost.UseUrls($"http://*:80");
+            builder.WebHost.UseUrls($"http://*:80");
 
             var app = builder.Build();
 
