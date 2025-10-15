1d5e108b87338401c506cb74912af19ba36fb682
NguyenBaDat20 <boyprocopodepgai@gmail.com>
2025-10-15 11:07:00 +0700

    feat(CRUDProduct): Move Product CRUD logic from Service to Repository layer

--- Repositories/ProductRepositories/IProductRepository.cs
+++ Repositories/ProductRepositories/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using BusinessObject.Models;
+﻿using BusinessObject.DTOs.ProductDto;
+using BusinessObject.Models;
 using Repositories.RepositoryBase;
 using System;
 using System.Collections.Generic;
@@ -10,8 +11,13 @@ namespace Repositories.ProductRepositories
 {
     public interface IProductRepository : IRepository<Product>
     {
+        IQueryable<Product> GetAllWithIncludes();
         Task<IEnumerable<Product>> GetProductsWithImagesAsync();
         Task<Product?> GetProductWithImagesByIdAsync(Guid id);
         Task<bool> IsProductAvailable(Guid productId, DateTime startDate, DateTime endDate);
+        Task<Product> AddProductWithImagesAsync(ProductRequestDTO dto);
+        Task<bool> UpdateProductWithImagesAsync(ProductDTO productDto);
+        Task<bool> UpdateProductStatusAsync(ProductStatusUpdateDto request);
+        Task<bool> HasOrderItemsAsync(Guid productId);
     }
 }
